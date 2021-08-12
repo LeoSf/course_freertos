7c8bc6dbc85d8eb2aee42b668c0406ee0cceb965
Leandro Medus <leomedus@gmail.com>
2021-08-12 17:37:35 +0200

    [mod] adding some comments for documentation

--- workspace_rtos_cube/stm32_helloworld/Core/Src/main.c
+++ workspace_rtos_cube/stm32_helloworld/Core/Src/main.c
@@ -2,17 +2,14 @@
 /**
   ******************************************************************************
   * @file           : main.c
+	* @author 				: Leandro D. Medus
   * @brief          : Main program body
   ******************************************************************************
   * @attention
   *
-  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+  * <h2><center>&copy; Copyright (c) 2021 Leandro Medus.
   * All rights reserved.</center></h2>
   *
-  * This software component is licensed by ST under BSD 3-Clause license,
-  * the "License"; You may not use this file except in compliance with the
-  * License. You may obtain a copy of the License at:
-  *                        opensource.org/licenses/BSD-3-Clause
   *
   ******************************************************************************
   */
@@ -383,6 +380,10 @@ static void MX_GPIO_Init(void)
 
 /* USER CODE BEGIN 4 */
 
+/**
+  * @brief  Basic blick function to check all leds present in the board
+  * @retval None
+  */
 static void blinky()
 {
 	static int idx = 0;
