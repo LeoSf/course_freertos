51292256c23d91ec22215f6af6bec1fadfd8b67f
Leandro Medus <leomedus@gmail.com>
2021-09-15 21:49:40 +0200

    [mod] poc_uart: sending a string

--- workspace_rtos_cubeide/poc_uart/Core/Inc/main.h
+++ workspace_rtos_cubeide/poc_uart/Core/Inc/main.h
@@ -44,6 +44,8 @@ extern "C" {
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
+#include <stdio.h>
+
 /* USER CODE END Includes */
 
 /* Exported types ------------------------------------------------------------*/
