d387a3c9466f70c1cfa8c6133708846200022c5a
Leandro Medus <leomedus@gmail.com>
2021-10-18 17:49:10 +0200

    [add] adding example to use FreeRTOS with colaborative mode

---
+++ workspace_rtos_cubeide/poc_uart/Core/Src/main_colaborative.c
@@ -0,0 +1,624 @@
+/* USER CODE BEGIN Header */
+/**
+    ******************************************************************************
+    * @file             : main.c
+    * @brief            : UART Proof of Concept
+    * @author           : Leo Medus
+    ******************************************************************************
+    * @details This program uses the UART of the NUCLEO-L552 board to send some
+    * string messages. The main program runs a FreeRTOS Kernel V10.3.1 with two tasks.
+    *
+    * @attention
+    *
+    * This software component is licensed by ST under BSD 3-Clause license,
+    * the "License"; You may not use this file except in compliance with the
+    * License. You may obtain a copy of the License at:
+    *                        opensource.org/licenses/BSD-3-Clause
+    *
+    ******************************************************************************
+    */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "cmsis_os.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "FreeRTOS.h"
+#include "task.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+#define TRUE            1
+#define FALSE           0
+#define AVAILABLE       TRUE
+#define NOT_AVAILABLE   FALSE
+
+#define DWT_CTRL    (*(volatile uint32_t*)0xE0001000)       /* Data Watchpoint and Trace Unit */
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+ADC_HandleTypeDef hadc1;
+
+UART_HandleTypeDef hlpuart1;
+
+RTC_HandleTypeDef hrtc;
+
+PCD_HandleTypeDef hpcd_USB_FS;
+
+/* USER CODE BEGIN PV */
+
+char usr_msg[250];
+uint8_t UART_ACCESS_KEY = AVAILABLE;
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_ADC1_Init(void);
+static void MX_LPUART1_UART_Init(void);
+static void MX_RTC_Init(void);
+static void MX_UCPD1_Init(void);
+static void MX_USB_PCD_Init(void);
+/* USER CODE BEGIN PFP */
+
+void sendString(char *msg);
+void sendChar(char *c);
+
+static void task1_handler(void* parameters);
+static void task2_handler(void* parameters);
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+    * @brief  The application entry point.
+    * @retval int
+    */
+int main(void)
+{
+    /* USER CODE BEGIN 1 */
+    TaskHandle_t task1_handle;
+    TaskHandle_t task2_handle;
+
+    BaseType_t status;
+
+    /* USER CODE END 1 */
+
+    /* MCU Configuration--------------------------------------------------------*/
+
+    /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+    HAL_Init();
+
+    /* USER CODE BEGIN Init */
+
+    /* USER CODE END Init */
+
+    /* Configure the system clock */
+    SystemClock_Config();
+
+    /* USER CODE BEGIN SysInit */
+
+    /* USER CODE END SysInit */
+
+    /* Initialize all configured peripherals */
+    MX_GPIO_Init();
+    MX_ADC1_Init();
+    MX_LPUART1_UART_Init();
+    MX_RTC_Init();
+    MX_UCPD1_Init();
+    MX_USB_PCD_Init();
+    /* USER CODE BEGIN 2 */
+
+
+    //Enable the CYCCNT counter.
+    DWT_CTRL |= ( 1 << 0);
+
+    // sending only one char
+    //sendChar((char*)"c");
+
+    sendString((char*)"[info] ---- Proof of Concept: UART in NUCLEO-L552----\r\n");
+
+
+//    SEGGER_UART_init(500000);
+
+    SEGGER_SYSVIEW_Conf();
+
+   // Start recording with SEGGER
+   SEGGER_SYSVIEW_Start();
+
+    status = xTaskCreate(
+                task1_handler,                  // name of the task handler
+                "Task-1",                       // descriptive name. (Could be NULL)
+                200,                            // stack space ([words] = 4*words [bytes])
+                "Hello world from Task-1",      // pvParameters
+                2,                              // priority of the task
+                &task1_handle);                 // handler to the TCB (task controller block)
+
+    configASSERT(status == pdPASS);
+
+    status = xTaskCreate(
+                task2_handler,
+                "Task-2",
+                200,
+                "Hello world from Task-2",
+                2,
+                &task2_handle);
+
+    configASSERT(status == pdPASS);
+
+    //start the freeRTOS scheduler
+    vTaskStartScheduler();
+
+    //if the control comes here, then the launch of the scheduler has failed due to
+    //insufficient memory in heap
+
+
+    /* USER CODE END 2 */
+
+    /* Infinite loop */
+    /* USER CODE BEGIN WHILE */
+    while (1)
+    {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+    }
+    /* USER CODE END 3 */
+}
+
+/**
+    * @brief System Clock Configuration
+    * @retval None
+    */
+void SystemClock_Config(void)
+{
+    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+    /** Configure the main internal regulator output voltage
+    */
+    if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE2) != HAL_OK)
+    {
+    Error_Handler();
+    }
+    /** Configure LSE Drive Capability
+    */
+    HAL_PWR_EnableBkUpAccess();
+    __HAL_RCC_LSEDRIVE_CONFIG(RCC_LSEDRIVE_LOW);
+    /** Initializes the RCC Oscillators according to the specified parameters
+    * in the RCC_OscInitTypeDef structure.
+    */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI48|RCC_OSCILLATORTYPE_HSI
+                                |RCC_OSCILLATORTYPE_LSE;
+    RCC_OscInitStruct.LSEState = RCC_LSE_ON;
+    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+    RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
+    RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+    {
+        Error_Handler();
+    }
+    /** Initializes the CPU, AHB and APB buses clocks
+    */
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                                |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
+    {
+        Error_Handler();
+    }
+}
+
+/**
+    * @brief ADC1 Initialization Function
+    * @param None
+    * @retval None
+    */
+static void MX_ADC1_Init(void)
+{
+
+    /* USER CODE BEGIN ADC1_Init 0 */
+
+    /* USER CODE END ADC1_Init 0 */
+
+    ADC_MultiModeTypeDef multimode = {0};
+    ADC_ChannelConfTypeDef sConfig = {0};
+
+    /* USER CODE BEGIN ADC1_Init 1 */
+
+    /* USER CODE END ADC1_Init 1 */
+    /** Common config
+    */
+    hadc1.Instance = ADC1;
+    hadc1.Init.ClockPrescaler = ADC_CLOCK_ASYNC_DIV1;
+    hadc1.Init.Resolution = ADC_RESOLUTION_12B;
+    hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+    hadc1.Init.ScanConvMode = ADC_SCAN_DISABLE;
+    hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+    hadc1.Init.LowPowerAutoWait = DISABLE;
+    hadc1.Init.ContinuousConvMode = DISABLE;
+    hadc1.Init.NbrOfConversion = 1;
+    hadc1.Init.DiscontinuousConvMode = DISABLE;
+    hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+    hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+    hadc1.Init.DMAContinuousRequests = DISABLE;
+    hadc1.Init.Overrun = ADC_OVR_DATA_PRESERVED;
+    hadc1.Init.OversamplingMode = DISABLE;
+    if (HAL_ADC_Init(&hadc1) != HAL_OK)
+    {
+        Error_Handler();
+    }
+    /** Configure the ADC multi-mode
+    */
+    multimode.Mode = ADC_MODE_INDEPENDENT;
+    if (HAL_ADCEx_MultiModeConfigChannel(&hadc1, &multimode) != HAL_OK)
+    {
+    Error_Handler();
+    }
+    /** Configure Regular Channel
+    */
+    sConfig.Channel = ADC_CHANNEL_3;
+    sConfig.Rank = ADC_REGULAR_RANK_1;
+    sConfig.SamplingTime = ADC_SAMPLETIME_2CYCLES_5;
+    sConfig.SingleDiff = ADC_SINGLE_ENDED;
+    sConfig.OffsetNumber = ADC_OFFSET_NONE;
+    sConfig.Offset = 0;
+    if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+    {
+        Error_Handler();
+    }
+    /* USER CODE BEGIN ADC1_Init 2 */
+
+    /* USER CODE END ADC1_Init 2 */
+
+}
+
+/**
+    * @brief LPUART1 Initialization Function
+    * @param None
+    * @retval None
+    */
+static void MX_LPUART1_UART_Init(void)
+{
+
+    /* USER CODE BEGIN LPUART1_Init 0 */
+
+    /* USER CODE END LPUART1_Init 0 */
+
+    /* USER CODE BEGIN LPUART1_Init 1 */
+
+    /* USER CODE END LPUART1_Init 1 */
+    hlpuart1.Instance = LPUART1;
+    hlpuart1.Init.BaudRate = 115200;
+    hlpuart1.Init.WordLength = UART_WORDLENGTH_8B;
+    hlpuart1.Init.StopBits = UART_STOPBITS_1;
+    hlpuart1.Init.Parity = UART_PARITY_NONE;
+    hlpuart1.Init.Mode = UART_MODE_TX_RX;
+    hlpuart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+    hlpuart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+    hlpuart1.Init.ClockPrescaler = UART_PRESCALER_DIV1;
+    hlpuart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+    hlpuart1.FifoMode = UART_FIFOMODE_DISABLE;
+    if (HAL_UART_Init(&hlpuart1) != HAL_OK)
+    {
+        Error_Handler();
+    }
+    if (HAL_UARTEx_SetTxFifoThreshold(&hlpuart1, UART_TXFIFO_THRESHOLD_1_8) != HAL_OK)
+    {
+        Error_Handler();
+    }
+    if (HAL_UARTEx_SetRxFifoThreshold(&hlpuart1, UART_RXFIFO_THRESHOLD_1_8) != HAL_OK)
+    {
+        Error_Handler();
+    }
+    if (HAL_UARTEx_DisableFifoMode(&hlpuart1) != HAL_OK)
+    {
+        Error_Handler();
+    }
+    /* USER CODE BEGIN LPUART1_Init 2 */
+
+    /* USER CODE END LPUART1_Init 2 */
+
+}
+
+/**
+    * @brief RTC Initialization Function
+    * @param None
+    * @retval None
+    */
+static void MX_RTC_Init(void)
+{
+
+    /* USER CODE BEGIN RTC_Init 0 */
+
+    /* USER CODE END RTC_Init 0 */
+
+    RTC_PrivilegeStateTypeDef privilegeState = {0};
+
+    /* USER CODE BEGIN RTC_Init 1 */
+
+    /* USER CODE END RTC_Init 1 */
+    /** Initialize RTC Only
+    */
+    hrtc.Instance = RTC;
+    hrtc.Init.HourFormat = RTC_HOURFORMAT_24;
+    hrtc.Init.AsynchPrediv = 127;
+    hrtc.Init.SynchPrediv = 255;
+    hrtc.Init.OutPut = RTC_OUTPUT_DISABLE;
+    hrtc.Init.OutPutRemap = RTC_OUTPUT_REMAP_NONE;
+    hrtc.Init.OutPutPolarity = RTC_OUTPUT_POLARITY_HIGH;
+    hrtc.Init.OutPutType = RTC_OUTPUT_TYPE_OPENDRAIN;
+    hrtc.Init.OutPutPullUp = RTC_OUTPUT_PULLUP_NONE;
+    if (HAL_RTC_Init(&hrtc) != HAL_OK)
+    {
+        Error_Handler();
+    }
+    privilegeState.rtcPrivilegeFull = RTC_PRIVILEGE_FULL_NO;
+    privilegeState.backupRegisterPrivZone = RTC_PRIVILEGE_BKUP_ZONE_NONE;
+    privilegeState.backupRegisterStartZone2 = RTC_BKP_DR0;
+    privilegeState.backupRegisterStartZone3 = RTC_BKP_DR0;
+    if (HAL_RTCEx_PrivilegeModeSet(&hrtc, &privilegeState) != HAL_OK)
+    {
+        Error_Handler();
+    }
+    /* USER CODE BEGIN RTC_Init 2 */
+
+    /* USER CODE END RTC_Init 2 */
+
+}
+
+/**
+    * @brief UCPD1 Initialization Function
+    * @param None
+    * @retval None
+    */
+static void MX_UCPD1_Init(void)
+{
+
+    /* USER CODE BEGIN UCPD1_Init 0 */
+
+    /* USER CODE END UCPD1_Init 0 */
+
+    LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+    /* Peripheral clock enable */
+    LL_APB1_GRP2_EnableClock(LL_APB1_GRP2_PERIPH_UCPD1);
+
+    LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOB);
+    LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOA);
+    /**UCPD1 GPIO Configuration
+    PB15   ------> UCPD1_CC2
+    PA15 (JTDI)   ------> UCPD1_CC1
+    */
+    GPIO_InitStruct.Pin = LL_GPIO_PIN_15;
+    GPIO_InitStruct.Mode = LL_GPIO_MODE_ANALOG;
+    GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+    LL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = LL_GPIO_PIN_15;
+    GPIO_InitStruct.Mode = LL_GPIO_MODE_ANALOG;
+    GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+    LL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    /* USER CODE BEGIN UCPD1_Init 1 */
+
+    /* USER CODE END UCPD1_Init 1 */
+    /* USER CODE BEGIN UCPD1_Init 2 */
+
+    /* USER CODE END UCPD1_Init 2 */
+
+}
+
+/**
+    * @brief USB Initialization Function
+    * @param None
+    * @retval None
+    */
+static void MX_USB_PCD_Init(void)
+{
+
+    /* USER CODE BEGIN USB_Init 0 */
+
+    /* USER CODE END USB_Init 0 */
+
+    /* USER CODE BEGIN USB_Init 1 */
+
+    /* USER CODE END USB_Init 1 */
+    hpcd_USB_FS.Instance = USB;
+    hpcd_USB_FS.Init.dev_endpoints = 8;
+    hpcd_USB_FS.Init.speed = PCD_SPEED_FULL;
+    hpcd_USB_FS.Init.phy_itface = PCD_PHY_EMBEDDED;
+    hpcd_USB_FS.Init.Sof_enable = DISABLE;
+    hpcd_USB_FS.Init.low_power_enable = DISABLE;
+    hpcd_USB_FS.Init.lpm_enable = DISABLE;
+    hpcd_USB_FS.Init.battery_charging_enable = DISABLE;
+    if (HAL_PCD_Init(&hpcd_USB_FS) != HAL_OK)
+    {
+    Error_Handler();
+    }
+    /* USER CODE BEGIN USB_Init 2 */
+
+    /* USER CODE END USB_Init 2 */
+
+}
+
+/**
+    * @brief GPIO Initialization Function
+    * @param None
+    * @retval None
+    */
+static void MX_GPIO_Init(void)
+{
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+    /* GPIO Ports Clock Enable */
+    __HAL_RCC_GPIOC_CLK_ENABLE();
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+    __HAL_RCC_GPIOG_CLK_ENABLE();
+    HAL_PWREx_EnableVddIO2();
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+
+    /*Configure GPIO pin Output Level */
+    HAL_GPIO_WritePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin, GPIO_PIN_RESET);
+
+    /*Configure GPIO pin Output Level */
+    HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, GPIO_PIN_RESET);
+
+    /*Configure GPIO pin Output Level */
+    HAL_GPIO_WritePin(GPIOB, UCPD_DBN_Pin|LED_BLUE_Pin, GPIO_PIN_RESET);
+
+    /*Configure GPIO pin : UCPD_FLT_Pin */
+    GPIO_InitStruct.Pin = UCPD_FLT_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(UCPD_FLT_GPIO_Port, &GPIO_InitStruct);
+
+    /*Configure GPIO pin : LED_GREEN_Pin */
+    GPIO_InitStruct.Pin = LED_GREEN_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(LED_GREEN_GPIO_Port, &GPIO_InitStruct);
+
+    /*Configure GPIO pin : LED_RED_Pin */
+    GPIO_InitStruct.Pin = LED_RED_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(LED_RED_GPIO_Port, &GPIO_InitStruct);
+
+    /*Configure GPIO pins : UCPD_DBN_Pin LED_BLUE_Pin */
+    GPIO_InitStruct.Pin = UCPD_DBN_Pin|LED_BLUE_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 4 */
+
+void sendString(char *msg)
+{
+//  sprintf(usr_msg, "[Test] -- sending a message --\r\n");
+    sprintf(usr_msg, msg);
+
+    // sending a string
+    HAL_UART_Transmit(&hlpuart1, (uint8_t *) usr_msg, strlen(usr_msg), 100);
+
+//	/* Init tickstart for timeout management */
+//	uint32_t tickstart = HAL_GetTick();
+//	// waiting for transmission to complete
+//	while(UART_WaitOnFlagUntilTimeout(&hlpuart1, UART_FLAG_TC, RESET, tickstart, HAL_UART_TIMEOUT_VALUE) != HAL_OK);
+//
+//	HAL_UART_Transmit(&hlpuart1, (uint8_t *) "done\n\r", 6, 100);
+}
+
+void sendChar(char *ch)
+{
+    // basic test to send a char
+    HAL_UART_Transmit(&hlpuart1, (uint8_t *) ch, 1, 100);
+}
+
+static void task1_handler(void* parameters)
+{
+
+    char msg[100];
+
+    while(1)
+    {
+        if(UART_ACCESS_KEY == AVAILABLE)
+        {
+            UART_ACCESS_KEY = NOT_AVAILABLE;
+            snprintf(msg,100,"%s\n", (char*)parameters);
+            sendString(msg);
+            UART_ACCESS_KEY = AVAILABLE;
+
+            /* segger info message for Segger SystemView */
+            SEGGER_SYSVIEW_PrintfTarget("Task-1 is yielding");
+
+//            traceISR_EXIT_TO_SCHEDULER();
+            taskYIELD();            // Macro for forcing a context switch
+        }
+    }
+}
+
+
+static void task2_handler(void* parameters)
+{
+    char msg[100];
+
+    while(1)
+    {
+        if(UART_ACCESS_KEY == AVAILABLE)
+        {
+            UART_ACCESS_KEY = NOT_AVAILABLE;
+            snprintf(msg,100,"%s\n", (char*)parameters);
+            sendString(msg);
+            UART_ACCESS_KEY = AVAILABLE;
+
+            /* segger info message for Segger SystemView */
+            SEGGER_SYSVIEW_PrintfTarget("Task-2 is yielding");
+
+//            traceISR_EXIT_TO_SCHEDULER();
+            taskYIELD();            // Macro for forcing a context switch
+        }
+    }
+}
+
+/* USER CODE END 4 */
+
+/**
+    * @brief  This function is executed in case of error occurrence.
+    * @retval None
+    */
+void Error_Handler(void)
+{
+    /* USER CODE BEGIN Error_Handler_Debug */
+    /* User can add his own implementation to report the HAL error return state */
+    __disable_irq();
+    while (1)
+    {
+    }
+    /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+    * @brief  Reports the name of the source file and the source line number
+    *         where the assert_param error has occurred.
+    * @param  file: pointer to the source file name
+    * @param  line: assert_param error line source number
+    * @retval None
+    */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+    /* USER CODE BEGIN 6 */
+    /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+    /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
