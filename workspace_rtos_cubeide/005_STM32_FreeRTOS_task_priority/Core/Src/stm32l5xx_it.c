11cafc255e0cf49db90719c4329658bddcb2a2e9
Leandro Medus <leomedus@gmail.com>
2021-11-05 09:29:45 +0100

    [mod] removing automatic code generation for interrupts since we are including FreeRTOS

--- workspace_rtos_cubeide/005_STM32_FreeRTOS_task_priority/Core/Src/stm32l5xx_it.c
+++ workspace_rtos_cubeide/005_STM32_FreeRTOS_task_priority/Core/Src/stm32l5xx_it.c
@@ -140,19 +140,6 @@ void UsageFault_Handler(void)
 }
 
 /**
- * @brief This function handles System service call via SWI instruction.
- */
-void SVC_Handler(void)
-{
-    /* USER CODE BEGIN SVCall_IRQn 0 */
-
-    /* USER CODE END SVCall_IRQn 0 */
-    /* USER CODE BEGIN SVCall_IRQn 1 */
-
-    /* USER CODE END SVCall_IRQn 1 */
-}
-
-/**
  * @brief This function handles Debug monitor.
  */
 void DebugMon_Handler(void)
@@ -165,55 +152,12 @@ void DebugMon_Handler(void)
     /* USER CODE END DebugMonitor_IRQn 1 */
 }
 
-/**
- * @brief This function handles Pendable request for system service.
- */
-void PendSV_Handler(void)
-{
-    /* USER CODE BEGIN PendSV_IRQn 0 */
-
-    /* USER CODE END PendSV_IRQn 0 */
-    /* USER CODE BEGIN PendSV_IRQn 1 */
-
-    /* USER CODE END PendSV_IRQn 1 */
-}
-
-/**
- * @brief This function handles System tick timer.
- */
-void SysTick_Handler(void)
-{
-    /* USER CODE BEGIN SysTick_IRQn 0 */
-
-    /* USER CODE END SysTick_IRQn 0 */
-    HAL_IncTick();
-    /* USER CODE BEGIN SysTick_IRQn 1 */
-
-    /* USER CODE END SysTick_IRQn 1 */
-}
-
 /******************************************************************************/
 /* STM32L5xx Peripheral Interrupt Handlers                                    */
 /* Add here the Interrupt Handlers for the used peripherals.                  */
 /* For the available peripheral interrupt handler names,                      */
 /* please refer to the startup file (startup_stm32l5xx.s).                    */
 /******************************************************************************/
-
-/**
- * @brief This function handles EXTI line13 interrupt.
- */
-void EXTI13_IRQHandler(void)
-{
-    /* USER CODE BEGIN EXTI13_IRQn 0 */
-
-    /* USER CODE END EXTI13_IRQn 0 */
-    HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_13);
-    /* USER CODE BEGIN EXTI13_IRQn 1 */
-
-    //    HAL_GPIO_TogglePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin);
-
-    /* USER CODE END EXTI13_IRQn 1 */
-}
 
 /* USER CODE BEGIN 1 */
 
